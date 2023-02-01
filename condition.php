d2a1831da9ed84bbcf03b50edf7bf135108e497c
Abdikamal127 <Abdikamal127@gmial.com>
2023-02-01 08:11:17 -0500

    Done

---
+++ condition.php
@@ -0,0 +1,17 @@
+<?php
+$grad = 96;
+
+if($grad > 95){
+    echo "A+";
+}else if($grad >85){
+    echo "A";
+}else if($grad >65){
+    echo "B";
+}else if($grad <50){
+    echo "Failed";
+}else{
+    echo "Waanu ka xunnahay macmiil lama fulin adeegaaga";
+}
+
+
+?>
